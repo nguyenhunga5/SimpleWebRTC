1217f9a69c92ee97b749710ae9ea9204cdad6f1f
Harry Nguyen <nguyenthanhhung19872004@gmail.com>
2017-07-23 12:10:12 +0700

    Create server socket

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+const fs = require('fs');
+const express = require('express');
+const https = require('https');
+
+const options = {
+    key: fs.readFileSync(__dirname + 'key.pem'),
+    cert: fs.readFileSync(__dirname + 'cert.pem')
+};
+
+var app = express();
+app.use(__dirname + '/out');
+
+var server = https.createServer(options, app);
+const io = require('socket.io').listen(server);
+server.listen(8080);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/test/index.html');
+})
